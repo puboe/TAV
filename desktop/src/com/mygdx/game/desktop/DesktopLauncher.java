740d832f07706719d14b24dd873ee3bb3e5dd130
Nacho Rivera <rivieng@gmail.com>
2015-11-28 18:09:39 -0300

    added shadow spotlight scene

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,11 +3,12 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.ShadowSpotlightScene;
 import com.mygdx.game.SpotLightScene;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new SpotLightScene(), config);
+        new LwjglApplication(new ShadowSpotlightScene(), config);
     }
 }
