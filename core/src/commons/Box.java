95baa597cc3389f1ed14df096855c83dd2eaf9f8
Nacho Rivera <rivieng@gmail.com>
2015-11-28 18:05:44 -0300

    Added box

---
+++ core/src/commons/Box.java
@@ -0,0 +1,16 @@
+package commons;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by riveign on 11/28/15.
+ */
+public class Box extends GameObject {
+
+    public Box(Vector3 position, Vector3 rotation, Vector3 scale){
+        this.position = position;
+        this. rotation = rotation;
+        this.scale = scale;
+    }
+}
